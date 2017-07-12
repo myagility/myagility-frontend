a68173da1c51d0ac42de3e7fd3ad549f19f5149b
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2017-07-12 15:46:40 +0200

    Add Users component with dummydata from github

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import Switch from 'react-router-dom';
-import Route from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
--- src/Users.js
+++ src/Users.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import axios from 'axios';
+import './Users.css';
 
 class Users extends React.Component {
 
-  render () {
-    return (
+  constructor() {
+    super();
+    this.state = { users: [] };
+  }
 
+  componentDidMount() {
+    axios.get('https://api.github.com/users')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({users: response.data});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const renderedUsers = this.state.users.map((user, index) => <li key={ index }><img src={ user.avatar_url } />{ user.login }</li>);
+    return (
+      <div>
+        <p></p>
+        <ul>
+          { renderedUsers }
+        </ul>
+      </div>
     )
   }
 }
+
+export default Users;
