9f43395ec50b02d3baf8d3591f03da2ff9c2dde0
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2017-07-12 15:05:16 +0200

    Add Login page and navbar

---
+++ src/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <a className="navbar-brand" href="#">myAgility</a>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
